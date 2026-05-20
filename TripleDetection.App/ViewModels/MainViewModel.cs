ed263d3d6b7ba78dadb444362af545ee24055a17
Malu <18102825911@189.cn>
2026-05-20 10:45:18 +0800

    feat: update MainViewModel with ObservableCollection for logs and results

--- TripleDetection.App/ViewModels/MainViewModel.cs
+++ TripleDetection.App/ViewModels/MainViewModel.cs
@@ -1,35 +1,71 @@
+using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace TripleDetection.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         private string _resultText = "--";
+        private string _resultBackground = "#808080";
+        private string _details = "Detection details will appear here";
+        private bool _isImageViewActive = true;
+        private string _selectedProcedure = "";
+
+        public ObservableCollection<string> LogMessages { get; } = new ObservableCollection<string>();
+        public ObservableCollection<string> ResultHistory { get; } = new ObservableCollection<string>();
+
         public string ResultText
         {
             get => _resultText;
-            set { _resultText = value; OnPropertyChanged(nameof(ResultText)); }
+            set { _resultText = value; OnPropertyChanged(); }
         }
 
-        private string _details = "Detection details will appear here";
+        public string ResultBackground
+        {
+            get => _resultBackground;
+            set { _resultBackground = value; OnPropertyChanged(); }
+        }
+
         public string Details
         {
             get => _details;
-            set { _details = value; OnPropertyChanged(nameof(Details)); }
+            set { _details = value; OnPropertyChanged(); }
         }
 
-        private bool _isLoading;
-        public bool IsLoading
+        public bool IsImageViewActive
         {
-            get => _isLoading;
-            set { _isLoading = value; OnPropertyChanged(nameof(IsLoading)); }
+            get => _isImageViewActive;
+            set { _isImageViewActive = value; OnPropertyChanged(); }
         }
 
-        protected void OnPropertyChanged(string name)
+        public string SelectedProcedure
+        {
+            get => _selectedProcedure;
+            set { _selectedProcedure = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void AddLog(string message)
+        {
+            var entry = $"[{DateTime.Now:HH:mm:ss}] {message}";
+            if (LogMessages.Count > 1000)
+                LogMessages.RemoveAt(0);
+            LogMessages.Add(entry);
+        }
+
+        public void AddResult(string result)
+        {
+            if (ResultHistory.Count > 500)
+                ResultHistory.RemoveAt(0);
+            ResultHistory.Add(result);
         }
     }
 }
