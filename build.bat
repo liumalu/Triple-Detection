7fdce8c7c10a58564bd53aea12cfc259da72dec9
Malu <18102825911@189.cn>
2026-05-30 20:41:58 +0800

    deleted the build shell

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d d:\xcm\Triple-Detection\TripleDetection.App
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" TripleDetection.App.csproj /t:Rebuild /p:Configuration=Debug /v:minimal
-echo Exit code: %ERRORLEVEL%
