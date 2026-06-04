9b4817d2cbfd4057b9d2db8c7d9017e5693367a2
Malu <18102825911@189.cn>
2026-06-04 10:23:09 +0800

    feat: add SaveToDb to VmSettingsService

--- Application/SettingsServices/VmSettingsService.cs
+++ Application/SettingsServices/VmSettingsService.cs
@@ -1,6 +1,9 @@
 using System;
 using System.IO;
+using Newtonsoft.Json;
+using TripleDetection.Domain.Entities;
 using TripleDetection.Infrastructure;
+using TripleDetection.Infrastructure.Repositories;
 using TripleDetection.Presentation.Models;
 
 namespace TripleDetection.Application.SettingsServices
@@ -9,6 +12,7 @@ namespace TripleDetection.Application.SettingsServices
 public class VmSettingsService
 {
     private readonly string _configPath;
+    private static readonly string DbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config", "tripledetection.db");
     private VmSettings _settings;
 
     public VmSettingsService()
@@ -26,6 +30,19 @@ public class VmSettingsService
     {
         _settings = settings;
         JsonHelper.Save(settings, _configPath);
+        SaveToDb(settings);
+    }
+
+    private void SaveToDb(VmSettings settings)
+    {
+        var systemConfig = new SystemConfig
+        {
+            Category = "VmSettings",
+            Key = "Settings",
+            Value = JsonConvert.SerializeObject(settings)
+        };
+        var repository = new SystemConfigRepository($"Data Source={DbPath}");
+        repository.SaveOrUpdate(systemConfig);
     }
 }
 }
