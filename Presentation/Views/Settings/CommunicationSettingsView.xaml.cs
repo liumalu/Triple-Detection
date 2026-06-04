08eb6cb3274ffabef264804988955879b84891a4
Malu <18102825911@189.cn>
2026-06-04 10:27:39 +0800

    feat: implement CommunicationSettingsView

--- Presentation/Views/Settings/CommunicationSettingsView.xaml.cs
+++ Presentation/Views/Settings/CommunicationSettingsView.xaml.cs
@@ -1,14 +1,12 @@
 using System.Windows.Controls;
+
 namespace TripleDetection.Presentation.Views.Settings
 {
     public partial class CommunicationSettingsView : UserControl
     {
-        public CommunicationSettingsView() : this((string)null) { }
-
-        public CommunicationSettingsView(string placeholder)
+        public CommunicationSettingsView()
         {
             InitializeComponent();
-            ContentArea.Text = "通讯设置功能待建设";
         }
     }
 }
