6d2890b6125cab01c215a8da78709b0c8bcf6ecc
Malu <18102825911@189.cn>
2026-06-04 10:23:21 +0800

    feat: add SaveToDb to DeviceControlSettingsService

--- Application/SettingsServices/DeviceControlSettingsService.cs
+++ Application/SettingsServices/DeviceControlSettingsService.cs
@@ -1,6 +1,9 @@
 using System;
 using System.IO;
+using Newtonsoft.Json;
+using TripleDetection.Domain.Entities;
 using TripleDetection.Infrastructure;
+using TripleDetection.Infrastructure.Repositories;
 using TripleDetection.Presentation.Models;
 
 namespace TripleDetection.Application.SettingsServices
@@ -10,6 +13,7 @@ public class DeviceControlSettingsService
 {
     private readonly string _configPath;
     private DeviceControlSettings _settings;
+    private static readonly string DbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config", "tripledetection.db");
 
     public DeviceControlSettingsService()
     {
@@ -26,6 +30,19 @@ public class DeviceControlSettingsService
     {
         _settings = settings;
         JsonHelper.Save(settings, _configPath);
+        SaveToDb(settings);
+    }
+
+    private void SaveToDb(DeviceControlSettings settings)
+    {
+        var config = new SystemConfig
+        {
+            Category = "DeviceControl",
+            Key = "Settings",
+            Value = JsonConvert.SerializeObject(settings)
+        };
+        var repository = new SystemConfigRepository($"Data Source={DbPath}");
+        repository.SaveOrUpdate(config);
     }
 }
 }
